b45009e5dd24a21b8f2f436f9c47b7bd77dbfaf7
Nikesh Tripathi <nikesh.tripathi@adityabirla.com>
2021-12-13 12:01:01 +0530

    cvldata api

--- web-service-gin/controllers/kyc.go
+++ web-service-gin/controllers/kyc.go
@@ -18,5 +18,25 @@ func (ctrl KycController) CVLApi(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"request": form})
+	if form.ApiName != "GET_CVL_DATA" {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"err": "wrong api details"})
+		return
+	}
+
+	var data = gin.H{
+		"kyc_name": "Test Yadav",
+		"pan": form.Request.PAN,
+		"kyc_status": "Y",
+		"msg" : "",
+	}
+
+	var response = gin.H{
+		"success": bool(true),
+		"response": gin.H{
+			"api_name" : form.ApiName,
+			"data" : data,
+		},
+	}
+
+	c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 }
