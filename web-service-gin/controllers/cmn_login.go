2b1d0b273ee6bfb2e7534a89448b4a5e54e8b04a
Dipesh Pithwa <dipesh@Dipeshs-MacBook-Air.local>
2021-12-14 14:48:57 +0530

    request and response changes

--- web-service-gin/controllers/cmn_login.go
+++ web-service-gin/controllers/cmn_login.go
@@ -4,52 +4,61 @@ import (
 	"mmi.com/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"mmi.com/forms"
 )
 
 type LoginController struct{}
 type ApiRequest struct{
-    PANNO string `json:"panno" binding:"required"`
+    PAN string `json:"pan" binding:"required,min=10,max=10"`
 }
+
 var LoginModel = new(models.LoginModel)
 func (ctrl LoginController) IsUserExists(c *gin.Context){
-	var request ApiRequest
-	c.BindJSON(&request)
-	results,err := LoginModel.IsUserExists(request.PANNO)
+	var form forms.RequestMaster
+	if validationErr := c.ShouldBindJSON(&form); validationErr != nil {
+		message := kycForm.GetCVLDataRequestValidate(validationErr)
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"message": message})
+		return
+	}
+	if form.ApiName != "IS_USER_EXISTS" {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"err": "wrong api details"})
+		return
+	}
+
+	print(form.Request.PAN)
+	results,err := LoginModel.IsUserExists(form.Request.PAN)
 
 	if err != nil {
 		var response = gin.H{
 			"success": bool(false),
-			"message": "Could not get data",
-			"data" : "",
-			"error": err,
+			"api_name": form.ApiName,
+			"data" : "Could not get data",
 		}
-		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"response": response})
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"api_response": response})
 		return
 	}
 
 	if len(results) > 0 {
 		var response = gin.H{
 			"success": bool(true),
-			"message": "Existing User",
-			"data": "",
-			"error": "",
+			"api_name": form.ApiName,
+			"data": "Existing User",
 		}
-		c.PureJSON(http.StatusOK, gin.H{"response": response})
+		c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 	}else{
 		var response = gin.H{
 			"success": bool(false),
-			"message": "New User",
-			"data": "",
-			"error": "",
+			"api_name": form.ApiName,
+			"data": "New User",
 		}
-		c.PureJSON(http.StatusOK, gin.H{"response": response})
+		c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 	}
 }
 
 func (ctrl LoginController) GetUserData(c *gin.Context){
 	var request ApiRequest
 	c.BindJSON(&request)
-	results,err := LoginModel.GetUserData(request.PANNO)
+	results,err := LoginModel.GetUserData(request.PAN)
 
 	if err != nil {
 		var response = gin.H{
@@ -58,25 +67,21 @@ func (ctrl LoginController) GetUserData(c *gin.Context){
 			"data": "",
 			"error" : err,
 		}
-		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"response": response})
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"api_response": response})
 		return
 	}
 
 	if len(results) > 0 {
 		var response = gin.H{
 			"success": bool(true),
-			"message": "Existing User",
 			"data": results[0],
-			"error" : "",
 		}
-		c.PureJSON(http.StatusOK, gin.H{"response": response})
+		c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 	}else{
 		var response = gin.H{
 			"success": bool(false),
-			"message": "Data Not Found",
-			"data": "",
-			"error" : "",
+			"data": "Data Not Found",
 		}
-		c.PureJSON(http.StatusOK, gin.H{"response": response})
+		c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 	}
 }
