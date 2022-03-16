a7ea7fc75b6dd2eb3a96f96f70a142ff116e28d8
Nikesh Tripathi <nikesh.tripathi@adityabirla.com>
2022-03-17 01:05:49 +0530

    onboarding api personal

--- web-service-gin/controllers/onboarding.go
+++ web-service-gin/controllers/onboarding.go
@@ -9,12 +9,17 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	ddb "mmi.com/db"
 	"context"
-	"log"
+	"encoding/json"
 )
 
 type OnboardingController struct{}
 
 var OboardingForm = new(forms.OboardingForm)
+type DynamoStructOnboarding struct {
+	PK          string `dynamodbav:"PK"`
+	SK          string `dynamodbav:"SK"`
+	Filler      string `dynamodbav:"Filler"`
+}
 
 func (ctrl OnboardingController) StorePersonalData(c *gin.Context) {
 	var form forms.RequestPersonalMaster
@@ -24,20 +29,37 @@ func (ctrl OnboardingController) StorePersonalData(c *gin.Context) {
 		return
 	}
 
-	if form.ApiName != "POST_PERSONAL_DATA" {
+	if form.ApiName != "STORE_PERSONAL_DATA" {
 		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"err": "API DETAILS INCORRECT"})
 		return
 	}
 
-
-	item := map[string]types.AttributeValue{
-		"PK":     &types.AttributeValueMemberS{Value: "AJCPT7078K"},
-		"SK":     &types.AttributeValueMemberS{Value: "3"},
-		"Filler": &types.AttributeValueMemberS{Value: form.Request},
-	}
-	err := putItem(d, tableName, item)
+	out, err := json.Marshal(form.Request)
 	if err != nil {
-		log.Fatal("failed to put item", err)
+		// panic(err)
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"err": err})
+	}
+
+	item := DynamoStructOnboarding{
+		"AJCPT7078O",
+		"11",
+		string(out),
+	}
+
+
+	// Testing PUTITEM CODE
+	// item := map[string]types.AttributeValue{
+	// 	"PK":     &types.AttributeValueMemberS{Value: "AJCPT7078Z"},
+	// 	"SK":     &types.AttributeValueMemberS{Value: "10"},
+	// 	"Filler": &types.AttributeValueMemberS{Value: string(out)},
+	// }
+
+	// fmt.Print(item)
+	// fmt.Print(d)
+
+	errL := UpdateItem(d, tableName, item)
+	if errL != nil {
+		c.AbortWithStatusJSON(http.StatusNotAcceptable, gin.H{"err": errL})
 	}
 
 	// var data = gin.H{
@@ -47,15 +69,15 @@ func (ctrl OnboardingController) StorePersonalData(c *gin.Context) {
 	// 	"msg" : "",
 	// }
 
-	// var response = gin.H{
-	// 	"success": bool(true),
-	// 	"response": gin.H{
-	// 		"api_name" : form.ApiName,
-	// 		"data" : data,
-	// 	},
-	// }
+	var response = gin.H{
+		"success": bool(true),
+		"response": gin.H{
+			"api_name" : form.ApiName,
+			"msg" : "data saved Successfully",
+		},
+	}
 
-	c.PureJSON(http.StatusOK, gin.H{"api_response": form.Request})
+	c.PureJSON(http.StatusOK, gin.H{"api_response": response})
 }
 
 func putItem(d *dynamodb.Client, tableName string, item map[string]types.AttributeValue) error {
@@ -64,5 +86,21 @@ func putItem(d *dynamodb.Client, tableName string, item map[string]types.Attribu
 		Item:                item,
 		ConditionExpression: aws.String("attribute_not_exists(PK)"),
 	})
+	return err
+}
+
+func UpdateItem(d *dynamodb.Client, tableName string, item DynamoStructOnboarding) error {
+	_, err := d.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]types.AttributeValue{
+			"PK": &types.AttributeValueMemberS{Value: item.PK},
+			"SK": &types.AttributeValueMemberS{Value: item.SK},
+		},
+		UpdateExpression: aws.String("set Filler = :Filler"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":Filler":        &types.AttributeValueMemberS{Value: item.Filler},
+		},
+	})
+
 	return err
 }
--- web-service-gin/go.mod
+++ web-service-gin/go.mod
@@ -3,6 +3,7 @@ module mmi/web-service-gin
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go v1.43.18 // indirect
 	github.com/aws/aws-sdk-go-v2/config v1.13.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.13.0 // indirect
 	github.com/gin-gonic/gin v1.7.6
@@ -13,7 +14,6 @@ require (
 	github.com/nbio/st v0.0.0-20140626010706-e9e8d9816f32 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
-	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/h2non/gentleman.v2 v2.0.5 // indirect
